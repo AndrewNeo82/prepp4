31e5b4f0b4a4739c991bc777b7fe8ca2956f2254
AndrewNeo82 <>
2024-01-12 13:41:49 +0000

    summernote

--- codestar/urls.py
+++ codestar/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django_summernote.admin import SummernoteModelAdmin
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
